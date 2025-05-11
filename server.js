5453054cdb82980d341315961c7da096096bf0be
Simone Diaco <s.diaco@datawizard.it>
2025-05-11 21:30:51 +0200

    edit for server

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  console.log(`Visit http://localhost:${PORT} to view the site`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+  console.log(`Visit http://0.0.0.0:${PORT} to view the site`);
 });
